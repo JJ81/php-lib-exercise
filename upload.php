d1e804e233335405dd4ea2eb33309e8b5f42ffad
yijaejun <yijaejun81@gmail.com>
2016-05-01 14:25:07 +0900

    upload multi files on the view

--- upload.php
+++ upload.php
@@ -1,4 +1,4 @@
 <form enctype="multipart/form-data" method="post" action="res_upload.php">
-	Upload image: <input type="file" id="file" name="file" /><br />
+	Upload image: <input type="file" id="file" name="file" multiple /><br />
 	<button type="submit" name="submit">Submit</button>
 </form>
